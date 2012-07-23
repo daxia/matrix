9dfda3eb982c1083b8eb7fd1c18998bf8e2d2bcc
wangshengzhong <632618392@qq.com>
2012-07-23 10:40:02 +0800

    new change

---
+++ src/InitTest.java
@@ -0,0 +1,28 @@
+
+
+/**
+ * 
+ * @description: ��֤δ����ʼ����String����������ʼ����String�ĳ�ʼ��ֵ ��null
+ *
+ * @package:  basic.init
+ * @filename: InitTest.java
+ * @author:   wangshengzhong
+ * @ctime:    2012-7-11
+ * @version:  v1.0
+ */
+public class InitTest {
+
+	private static  String str;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+//		new InitTest();
+		System.out.println(str);
+	}
+	
+	
+
+}
---
+++ src/InitTest2.java
@@ -0,0 +1,26 @@
+
+
+public class InitTest2 {
+
+	/**
+	 * @param args
+	 */
+	static String str1 = "wang";
+
+	public static void main(String[] args) {
+new Test();
+		System.out.println(str1);
+
+	}
+}
+
+class Test{
+	
+	Test(){
+		String str = "shengzhong";
+		System.out.println("str");
+	}
+	
+	String str2 = "wang";
+	
+}
