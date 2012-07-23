b7b7d7102b0489e9369434182883a57ec1b13b3c
wangshengzhong <632618392@qq.com>
2012-07-23 15:56:10 +0800

    change file : InitTest2.java

--- src/InitTest2.java
+++ src/InitTest2.java
@@ -1,5 +1,4 @@
 
-
 public class InitTest2 {
 
 	/**
@@ -8,19 +7,19 @@ public class InitTest2 {
 	static String str1 = "wang";
 
 	public static void main(String[] args) {
-new Test();
+		new Test1();
 		System.out.println(str1);
 
 	}
 }
 
-class Test{
-	
-	Test(){
+class Test1 {
+
+	Test1() {
 		String str = "shengzhong";
 		System.out.println("str");
 	}
-	
+
 	String str2 = "wang";
-	
+
 }
