5f847ee81f6249e64b707844499d16365fe80a24
632618392@qq.com <632618392@qq.com>
2012-07-25 21:16:11 +0800

    更新think in java  codes

--- src/think_in_java_E4/_03_initialization/practices/Practice13_StaticFiled.java
+++ src/think_in_java_E4/_03_initialization/practices/Practice13_StaticFiled.java
@@ -1,20 +1,45 @@
 package think_in_java_E4._03_initialization.practices;
 
+//practice13
+//static filed
+
+//the sequence of create object
+// static variable---> general variable --->constructor
+
 public class Practice13_StaticFiled {
 
 	public static void main(String[] args) {
-
+		Cups cups = new Cups();
+		cups.cup1.f(5);
+		cups.cup2.f(555);
 	}
 
 }
 
-class Cups{
+class Cups {
+	int i;
+	{
+		i = 5;
+		System.out.println(i);
+	}
 	static Cup cup1;
 	static Cup cup2;
+	static {
+		cup1 = new Cup(1);
+		cup2 = new Cup(2);
+	}
+	Cups(){
+		System.out.println("Cups");
+	}
 }
 
-class Cup{
-	
+class Cup {
+	Cup(int maker) {
+		System.out.println("Cup(" + maker + ")");
+	}
+
+	void f(int i) {
+		System.out.println("f(" + i + ")");
+	}
+
 }
-
-
---
+++ src/think_in_java_E4/_03_initialization/practices/Practice14_StaticInit.java
@@ -0,0 +1,20 @@
+package think_in_java_E4._03_initialization.practices;
+
+//practice14
+//static initialization
+public class Practice14_StaticInit {
+
+	public static void main(String[] args) {
+		System.out.println(StaticInit.str1);
+		System.out.println(StaticInit.str2);
+	}
+
+}
+
+class StaticInit {
+	static String str1 = "str1";
+	static String str2;
+	static {
+		str2 = "str2";
+	}
+}
